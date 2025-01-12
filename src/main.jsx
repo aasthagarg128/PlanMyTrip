f322eab1929017bcfd3fae14a7f705d0eb14db95
aasthagarg128 <aasthagarg128@gmail.com>
2025-01-12 17:26:09 +0530

    project setup complete

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css';
+import { createBrowserRouter } from 'react-router-dom';
+import { RouterProvider } from 'react-router';
+import CreateTrip from './create-trip/index.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const router=createBrowserRouter([
+  {
+    path:'/',
+    element:<App/>
+  },
+  {
+    path:'/create-trip',
+    element:<CreateTrip/>
+  }
+])
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>,
 )
